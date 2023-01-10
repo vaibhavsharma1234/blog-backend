a5d79bab9d13922155d8e86c7640f51bc47fab4c
vaibhav <vaibh>
2023-01-10 21:03:54 +0530

    new1

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ app.use(express.json({ extended: true }));
 app.use('/', Router);
 
 
-const PORT = 8000;
+// const PORT = 8000;
 const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
 
 Connection(username, password);
 
-app.listen(PORT, () => console.log(`Server is running successfully on PORT ${PORT}`));
+app.listen(process.env.PORT, () => console.log(`Server is running successfully on PORT ${PORT}`));
